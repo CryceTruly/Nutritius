50a5d30a96816dd529c8fcca32f694c711444ffb
crycetruly <crycetruly@gmail.com>
2019-02-12 20:29:54 +0300

    Added login/logout

--- nutritiusserver/nutritiusserver/settings.py
+++ nutritiusserver/nutritiusserver/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-  'users'
+  'users',
+  'foods',
+  'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -119,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL='home'
--- nutritiusserver/users/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import login,index
-urlpatterns=[
-    path('login/',login),
-        path('',index),
-]
