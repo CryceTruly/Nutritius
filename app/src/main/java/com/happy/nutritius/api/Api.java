6476d1e971e1fb7922d7f4ff0fdb09b0164b5a74
crycetruly <crycetruly@gmail.com>
2019-04-05 16:44:04 +0300

    ft made it better

--- app/src/main/java/com/happy/nutritius/api/Api.java
+++ app/src/main/java/com/happy/nutritius/api/Api.java
@@ -2,6 +2,6 @@ package com.happy.nutritius.api;
 
 public class Api {
 
-        public static final String BASE_URL = "t:8000/api/";
+        public static final String BASE_URL = "https://still-temple-42855.herokuapp.com/api/";
     }
 
